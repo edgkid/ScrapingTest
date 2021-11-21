2561e7cdde35558d84610793209cfbdbe2cb90ca
edgar <edglandaeta.15@gmail.com>
2021-11-21 18:56:16 -0400

    Cierre de session

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "main#home"
 
+  devise_scope :user do
+    get '/users/sign_out' => 'sessions#destroy'
+  end
+
 end
