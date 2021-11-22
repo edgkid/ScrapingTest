51982b1d272c511dafbf88afe7c89c04470a454c
edgar <edglandaeta.15@gmail.com>
2021-11-21 23:02:21 -0400

    config

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,12 @@ module TestScraping
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.initialize_on_precompile = false
+    onfig.time_zone = 'America/Caracas'
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = %i[es en]
+  
+    # config.assets.initialize_on_precompile = false
+    config.eager_load_paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts'
   end
 end
